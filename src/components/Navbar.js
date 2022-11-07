ef1c55e55943c53f9a19a115a195b7be06b9b2d1
SvkCreations <thesvk1@gmail.com>
2022-11-07 16:11:11 +0530

    more optimization

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar(props) {
     <div>
       <header>
         <div className="container-fluid rounded row-header" style={props.style}>
-          <div className="container align-items-center">
+          <div className="container align-items-center px-4 px-md-0">
             <div className="row justify-content-end">
               <div className="form-check form-switch col-auto">
                 <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onClick={props.toggleMode} />
@@ -13,12 +13,12 @@ export default function Navbar(props) {
             </div>
             <h1 className="display-4 fw-bold mt-3">Welcome to TextUtils</h1>
             <h2>The best text utilizing app around.</h2>
-            <p className="lead py-3 col-md-6">We provide you with so many options to utilize your text like counting words, letters, convert the text to uppercase and many more. You can also copy the converted text to your clipboard by just clicking the copy button.</p>
+            <p className="lead py-3 col-md-8">We provide you with so many options to utilize your text like counting words, letters, convert the text to uppercase and many more. You can also copy the converted text to your clipboard by just clicking the copy button.</p>
             <div className="row mt-3 gap-2 gap-md-0">
-              <div className="col-md-2">
+              <div className="col-md-auto">
                 <button className="btn btn-lg btn-warning col-12">Start utilizing</button>
               </div>
-              <div className="col-md-2">
+              <div className="col-md-auto">
                 <button className="btn btn-lg btn-outline-light col-12">Learn more</button>
               </div>
             </div>
--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -28,31 +28,45 @@ export default function TextArea(props) {
 
     return (
         <div>
-            <div className="container p-3">
+            <div className="container px-4 px-md-0">
                 <div className="row mt-4">
                     <h1 className="display-6 fw-bold">Type in any text to Analyze.</h1>
                 </div>
                 <div className="row d-flex">
-                    <div className="col-auto flex-grow-1">
-                        <p className="lead"><span style={{color:"#6200ee", fontWeight:"bold"}}>{text.length==0 || text.endsWith(' ')? (text.split(' ').length)-1 : text.split(' ').length}</span> Words, <span style={{color:"#6200ee", fontWeight:"bold"}}>{text.length}</span> Characters</p>
+                    <div className="col-auto flex-grow-1 align-self-center">
+                        <p className="lead"><span style={{ color: "#59319d", fontWeight: "bold" }}>{text.length == 0 || text.endsWith(' ') ? (text.split(' ').length) - 1 : text.split(' ').length}</span> Words, <span style={{ color: "#59319d", fontWeight: "bold" }}>{text.length}</span> Characters</p>
                     </div>
                     <div className="col-auto">
-                    <button className="btn" onClick={handleClrClick} style={props.scBtnStyle}>Clear text</button>
+                        <button className="btn" onClick={handleClrClick} style={props.scBtnStyle}>Clear text</button>
                     </div>
                 </div>
                 <div className="row mt-2">
-                    <div className="mb-3">
-                        <textarea className="form-control p-3" placeholder='Enter text here...' value={text} onChange={handleOnchange} id="exampleFormControlTextarea1" rows="10" style={props.inpStyle}></textarea>
+                    <div className="mb-4">
+                        <textarea className="form-control p-3" placeholder='Enter text here...' value={text} onChange={handleOnchange} id="exampleFormControlTextarea1" rows="10" style={props.inpStyle} spellCheck="true"></textarea>
                     </div>
                 </div>
-                <div className="row gap-1 d-flex align-items-center justify-content-center">
+                <div className="row gap-2 d-flex align-items-center justify-content-center mb-5">
                     <button className="btn col-5 col-md-2" onClick={handleUpClick} style={props.scBtnStyle}>Convert to Uppercase</button>
                     <button className="btn col-5 col-md-2" onClick={handleLowClick} style={props.scBtnStyle}>Convert to Lowercase</button>
                     <button className="btn col-5 col-md-2" onClick={handleEsClick} style={props.scBtnStyle}>Remove extra spaces</button>
-                    <button className="btn col-5 col-md-2 h-100" onClick={handleRsClick} style={props.scBtnStyle}>Remove all the spaces</button>
+                    <button className="btn col-5 col-md-2" onClick={handleRsClick} style={props.scBtnStyle}>Remove all the spaces</button>
                     <button className="btn col-5 col-md-2" style={props.prBtnStyle}>Copy text</button>
                 </div>
+
             </div>
+            <footer className='container my-5 px-4 px-md-0'>
+                <hr />
+                <div className="row">
+                    <div className="col-12 col-md-6">
+                        <p>Created by Soubhik Ghosh</p>
+                    </div>
+                    <div className="col col-md-auto icons ms-auto">
+                    <a href="facebook.com/"><iconify-icon icon="akar-icons:facebook-fill" style={{color:"#59319d", fontSize:"24px", marginRight:"12px"}}></iconify-icon></a>
+                    <a href="instagram.com/"><iconify-icon icon="akar-icons:instagram-fill"style={{color:"#59319d", fontSize:"24px", marginRight:"12px"}}></iconify-icon></a>
+                    <a href="tell:+91 8240703010"><iconify-icon icon="akar-icons:whatsapp-fill" style={{color:"#59319d", fontSize:"24px", marginRight:"12px"}}></iconify-icon></a>
+                    </div>
+                </div>
+            </footer>
         </div>
     )
 }
